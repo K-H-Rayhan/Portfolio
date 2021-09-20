7418304250c69f49240a19f1f24721817427bd55
Kamrul Hasan Rayhan <aahim7406@gmail.com>
2021-09-20 08:37:59 +0600

    commit on 'main'

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,7 +3,9 @@ import Layout from '../../components/layout/Layout'
 export default function index() {
     return (
         <Layout>
-            Blog
+            <div className=" h-screen flex flex-row items-center justify-center font-extrabold">
+                Woking on it :)
+            </div>
         </Layout>
     )
 }
--- pages/contact.js
+++ pages/contact.js
@@ -4,26 +4,25 @@ function contact() {
     return (
         <Layout title="Rayhan | Contact">
             <div className="md:h-screen w-full grid md:grid-cols-2 mt-16 md:mt-0 items-center justify-center justify-items-center">
-                <div className="xl:text-6xl lg:text-5xl text-4xl font-extrabold flex-wrap grid grid-rows-2 items-center  text-green-400 underline m-5">Contact Me</div>
-                <div className="flex items-center justify-center">
-                    <form action="submit" className=" flex flex-col">
-                        <input type="name" className="outline-none" placeholder="Name" />
-                        <input type="email" className="outline-none" placeholder="Email" />
-                        
-                        <input type="number" className="outline-none" placeholder="Number" />
-                        <textarea name="" id="" cols="30" rows="10" placeholder="Your Message" className="outline-none"></textarea>
-                        <div className="self-center"><Button name="Send Message" /></div>
+                <div className="xl:text-6xl lg:text-5xl text-4xl font-extrabold flex-wrap grid grid-rows-2 items-center  text-green-400 mx-5">Contact Me<hr className=" w-16 h-2 bg-green-400 rounded-full" /></div>
+                <div className="flex items-center justify-center w-full">
+                    <form action="submit" className=" flex flex-col w-full p-2 md:p-10 gap-3 md:gap-5" >
+                        <input type="name" name="name" className=" text-lg font-semibold focus:ring-1 focus:ring-green-400 outline-none rounded-md h-12 border-2 border-gray-300 focus:border-green-400 p-2" placeholder="Name" />
+                        <input type="name" name="email" className=" text-lg font-semibold focus:ring-1 focus:ring-green-400 outline-none  rounded-md h-12 border-2 border-gray-300 focus:border-green-400 p-2" placeholder="Email" />
+                        <input type="name" name="number" className=" text-lg  font-semibold focus:ring-1 focus:ring-green-400 outline-none  rounded-md h-12 border-2 border-gray-300 focus:border-green-400 p-2" placeholder="Contact No." />
+                        <textarea name="" id="" cols="30" rows="4" className=" text-lg font-semibold focus:ring-1 focus:ring-green-400 outline-none  rounded-md border-2 border-gray-300 focus:border-green-400 p-2" placeholder="Your Message"></textarea>
+                        <div className="self-center"><Button name="Send Message" className=" disabled:"/></div>
                     </form>
                 </div>
             </div>
             <div className="md:h-screen w-full mt-16 md:mt-0 grid grid-rows-5">
-                <div className="xl:text-6xl lg:text-5xl text-4xl font-extrabold text-green-400 underline m-5 self-center place-self-center justify-self-center">Other Contacts</div>
+                <div className="xl:text-6xl lg:text-5xl text-4xl font-extrabold text-green-400 mx-5 self-center place-self-center justify-self-center">Other Contacts<hr className="w-full h-2 bg-green-400 rounded-full" /></div>
                 <div className="grid grid-cols-2">
                     <div>
-                            <div>asdf</div>
-                            <div>asdf</div>
-                            <div>asdf</div>
-                            
+                        <div>asdf</div>
+                        <div>asdf</div>
+                        <div>asdf</div>
+
                     </div>
                     <div>
                         asdasd
