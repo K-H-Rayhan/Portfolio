6da68cbab1ef09cb2851eb7c259a7129d0ef8e58
Rayhan <aahim7406@gmail.com>
2021-11-18 16:08:36 +0600

    commit on 'main'

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ function about() {
     return (
         <Layout title="Rayhan | About">
             <div className=" h-screen flex flex-col justify-center items-center">
-            <div className="md:text-6xl text-4xl font-extrabold flex-wrap text-green-400 mx-5 md:row-start-2 mb-12">
+            <div className="md:text-6xl text-4xl font-extrabold flex-wrap text-green-400 mx-5 md:row-start-2 mb-8">
                     About Me<hr className="mt-3 w-16 h-2 bg-green-400 rounded-full" />
                 </div>
                 <div className=" flex flex-col mx-auto bg-white items-center">
@@ -12,7 +12,7 @@ function about() {
                         <div className="text-center font-semibold text-lg tracking-wide">Kamrul Hasan Rayhan</div>
                         <div className="text-center text-base">@khrayhan</div>
                     </div>
-                    <div className="w-full sm:w-2/5 lg:w-2/5 text-center mt-5 font-semibold md:border-t-2 border-gray-700 md:shadow-lg p-6 text-lg tracking-wide sm:rounded-xl" >
+                    <div className="w-full sm:w-2/5 lg:w-2/5 text-center mt-5 font-semibold  md:border-t-2 border-green-400 md:shadow-lg p-6 text-lg tracking-wide sm:rounded-xl" >
                         I'm a Full Stack Developer from Dhaka, Bangladesh. <br />
                         I always strive to write elegent and efficient code. Passionate about learning and exploring new technologies as
                         well as enthusiastic about UI/UX.<br />
@@ -24,7 +24,7 @@ function about() {
             <div className="md:text-6xl text-4xl font-extrabold flex-wrap text-green-400 mx-5 md:row-start-2">
                     Tech Skills<hr className="mt-3 w-16 h-2 bg-green-400 rounded-full" />
                 </div>
-                <div className="w-full sm:w-2/5 lg:w-2/5 text-center mt-5 font-semibold md:border-t-2 border-gray-700 md:shadow-lg p-6 text-lg tracking-wide sm:rounded-xl" >
+                <div className="w-full sm:w-2/5 lg:w-2/5 text-center mt-5 font-semibold md:border-t-2 border-green-400 md:shadow-lg p-6 text-lg tracking-wide sm:rounded-xl" >
                     <div className="font-extrabold text-lg tracking-wide " >Libraries & Frameworks: <span className="font-bold text-base tracking-tight  " >
                         React, NextJS, Tailwind, Bootstrap, ExpressJs</span><br /></div>
                     <div className="font-extrabold text-lg tracking-wide" >Databases: <span className="font-bold text-base tracking-tight " >
