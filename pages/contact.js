9289c16c3b6e35f1146862a9132d12075a11cfb8
kh-rayhan <97230670+kh-rayhan@users.noreply.github.com>
2022-02-09 19:42:05 +0600

    commit on 'main'

--- pages/contact.js
+++ pages/contact.js
@@ -26,10 +26,10 @@ export default function contact() {
 
   return (
     <Layout title="Rayhan | Contact">
-      <div class="bg-white py-20 lg:py-[120px] overflow-hidden relative md:w-auto m-4 md:h-screen">
+      <div class="bg-white py-20 lg:py-[120px] overflow-hidden relative m-4 md:h-screen md:w-3/4 flex content-center">
         <div class="container">
-          <div class="flex flex-wrap lg:justify-between -mx-4">
-            <div class="w-full lg:w-1/2 xl:w-6/12 px-4">
+          <div class="flex flex-wrap md:justify-between">
+            <div class="">
               <div class="max-w-[570px] mb-12 lg:mb-0">
                 <span class="block mb-4 text-base text-green-400 font-semibold">
                   Contact Me
@@ -172,33 +172,6 @@ export default function contact() {
         </div>
       </div>
 
-      {/* <div className="w-full h-screen grid grid-rows-5 md:grid-cols-2 mt-16 md:mt-0 items-center justify-center justify-items-center" >
-                <div className="md:text-6xl text-4xl font-extrabold flex-wrap text-green-400 mx-5 md:row-start-2 ">
-                    Contact Me<hr className="mt-3 w-16 h-2 bg-green-400 rounded-full " />
-                    <div className=" bg-fixed bg-contactBg w-full"></div>
-                </div>
-                
-                <div className="flex items-center justify-center w-full row-span-3 row-start-2">
-                    <form className=" flex flex-col w-full p-2 md:p-10 md:gap-5" onSubmit={handleSubmit}>
-                        <input type="text" name="username"
-                            value={inputs.username || ""}
-                            onChange={handleChange} className={inputDesign} placeholder="Name" required />
-                        <input type="email" name="email"
-                            value={inputs.email || ""}
-                            onChange={handleChange} className={inputDesign} placeholder="Email" required />
-                        <input type="phone" name="phone"
-                            value={inputs.phone || ""}
-                            onChange={handleChange} className={inputDesign} placeholder="Contact No." required />
-                        <textarea name="text"
-                            value={inputs.text || ""}
-                            onChange={handleChange} id="" cols="30" rows="4" className=" text-lg font-semibold focus:ring-1 focus:ring-green-400 outline-none  rounded-md border-2 border-gray-300 focus:border-green-400 p-2" placeholder="Your Message" required></textarea>
-                        <div className="self-center mt-6">
-                            <button type="submit" className="bg-green-400 hover:bg-green-500 text-sm text-white font-bold py-6 px-12 rounded-full tracking-widest transform duration-500 hover:-translate-y-4 mt-3 mb-10" onClick={done}>Submit</button>
-                        </div>
-                    </form>
-                </div>
-
-            </div> */}
       <div className=" grid md:grid-rows-4 justify-items-center">
         <div className="xl:text-6xl lg:text-5xl text-4xl font-extrabold text-green-400 mx-5 self-center place-self-center justify-self-center mt-0 md:mt-4">
           Contacts
@@ -241,3 +214,32 @@ export async function getStaticProps(context) {
     props: {}, // will be passed to the page component as props
   };
 }
+
+
+      {/* <div className="w-full h-screen grid grid-rows-5 md:grid-cols-2 mt-16 md:mt-0 items-center justify-center justify-items-center" >
+                <div className="md:text-6xl text-4xl font-extrabold flex-wrap text-green-400 mx-5 md:row-start-2 ">
+                    Contact Me<hr className="mt-3 w-16 h-2 bg-green-400 rounded-full " />
+                    <div className=" bg-fixed bg-contactBg w-full"></div>
+                </div>
+                
+                <div className="flex items-center justify-center w-full row-span-3 row-start-2">
+                    <form className=" flex flex-col w-full p-2 md:p-10 md:gap-5" onSubmit={handleSubmit}>
+                        <input type="text" name="username"
+                            value={inputs.username || ""}
+                            onChange={handleChange} className={inputDesign} placeholder="Name" required />
+                        <input type="email" name="email"
+                            value={inputs.email || ""}
+                            onChange={handleChange} className={inputDesign} placeholder="Email" required />
+                        <input type="phone" name="phone"
+                            value={inputs.phone || ""}
+                            onChange={handleChange} className={inputDesign} placeholder="Contact No." required />
+                        <textarea name="text"
+                            value={inputs.text || ""}
+                            onChange={handleChange} id="" cols="30" rows="4" className=" text-lg font-semibold focus:ring-1 focus:ring-green-400 outline-none  rounded-md border-2 border-gray-300 focus:border-green-400 p-2" placeholder="Your Message" required></textarea>
+                        <div className="self-center mt-6">
+                            <button type="submit" className="bg-green-400 hover:bg-green-500 text-sm text-white font-bold py-6 px-12 rounded-full tracking-widest transform duration-500 hover:-translate-y-4 mt-3 mb-10" onClick={done}>Submit</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div> */}
