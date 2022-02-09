62d334e3d525b2448dddecdb947cb3f3c7d6167f
kh-rayhan <97230670+kh-rayhan@users.noreply.github.com>
2022-02-09 13:40:56 +0600

    commit on 'main'

---
+++ pages/asd.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+function asd() {
+  const ref = React.useRef(null);
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = React.useState([]);
+  React.useEffect(() => {
+    fetchData();
+    import("@lottiefiles/lottie-player");
+  }, []);
+  const fetchData = async () => {
+    let petsData = await fetch("https://jsonplaceholder.typicode.com/photos");
+    let convertData = await petsData.json();
+    setData(convertData);
+    console.log(setData);
+    setTimeout(function () {
+      setLoading(false);
+    }, 5000);
+  };
+
+  return loading ? (
+    <div className=" w-full flex justify-content items-center">
+      <main>
+        <lottie-player
+          ref={ref}
+          src="https://assets5.lottiefiles.com/packages/lf20_szlepvdh.json"
+          background="transparent"
+          speed="1"
+          style={{ width: "300px", height: "300px" }}
+          loop
+          autoplay
+        ></lottie-player>
+      </main>
+    </div>
+  ) : (
+    <div>
+      {data.map((event, i) => {
+        return (
+          <div key={i}>
+            <img alt="s" src={event.url} width={200} height={200}></img>
+            <h1>
+              {i}.{event.title}
+            </h1>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default asd;
--- pages/contact.js
+++ pages/contact.js
@@ -1,29 +1,178 @@
-import Layout from '../components/layout/Layout'
-import Contactcard from '../components/card/Contactcard'
+import Layout from "../components/layout/Layout";
+import Contactcard from "../components/card/Contactcard";
 import { useState } from "react";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 export default function contact() {
-    const inputDesign = " text-lg font-semibold focus:ring-1 focus:ring-green-400 outline-none rounded-md h-12 border-2 border-gray-300 focus:border-green-400 p-2 md:mb-0 mb-3"
-    const [inputs, setInputs] = useState({});
-    const done = async (e) => {
-        const data = await fetch(`https://khrayhan.me/api/contact?username=${inputs.username}&email=${inputs.email}&phone=${inputs.phone}&message=${inputs.text}`)
-        const res = await data.json();
-        console.log(res);
-    }
-    const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setInputs(values => ({ ...values, [name]: value }))
-    }
-    const router = useRouter();
-    const handleSubmit = (event) => {
-        router.push('/thanks')
-        event.preventDefault();
-    }
+  const inputDesign =
+    " text-lg font-semibold focus:ring-1 focus:ring-green-400 outline-none rounded-md h-12 border-2 border-gray-300 focus:border-green-400 p-2 md:mb-0 mb-3";
+  const [inputs, setInputs] = useState({});
+  const done = async (e) => {
+    const data = await fetch(
+      `https://khrayhan.me/api/contact?username=${inputs.username}&email=${inputs.email}&phone=${inputs.phone}&message=${inputs.text}`
+    );
+    const res = await data.json();
+    console.log(res);
+  };
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+  const router = useRouter();
+  const handleSubmit = (event) => {
+    router.push("/thanks");
+    event.preventDefault();
+  };
 
-    return (
-        <Layout title="Rayhan | Contact">
-            <div className="w-full h-screen grid grid-rows-5 md:grid-cols-2 mt-16 md:mt-0 items-center justify-center justify-items-center" >
+  return (
+    <Layout title="Rayhan | Contact">
+      <div class="bg-white py-20 lg:py-[120px] overflow-hidden relative md:w-auto m-4 md:h-screen">
+        <div class="container">
+          <div class="flex flex-wrap lg:justify-between -mx-4">
+            <div class="w-full lg:w-1/2 xl:w-6/12 px-4">
+              <div class="max-w-[570px] mb-12 lg:mb-0">
+                <span class="block mb-4 text-base text-green-400 font-semibold">
+                  Contact Me
+                </span>
+                <h2
+                  class="
+                  text-dark
+                  mb-6
+                  uppercase
+                  font-bold
+                  text-[32px]
+                  sm:text-[40px]
+                  lg:text-[36px]
+                  xl:text-[40px]
+                  "
+                >
+                  GET IN TOUCH WITH ME
+                </h2>
+                {/* <p class="text-base text-body-color leading-relaxed mb-9">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eius tempor incididunt ut labore et dolore magna aliqua. Ut
+                  enim adiqua minim veniam quis nostrud exercitation ullamco
+                </p> */}
+              </div>
+            </div>
+            <div class="w-full lg:w-1/2 xl:w-5/12 px-4">
+              <div class="bg-white relative rounded-lg p-8 sm:p-12 shadow-lg">
+                <form
+                  onSubmit={handleSubmit}
+                >
+                  <div class="mb-6">
+                    <input
+                      type="text"
+                      name="username"
+                      value={inputs.username || ""}
+                      onChange={handleChange}
+                      placeholder="Name"
+                      required
+                      className="
+                        w-full
+                        rounded
+                        py-3
+                        px-[14px]
+                        text-body-color text-base
+                        border border-[lightgray]
+                        outline-none
+                        focus-visible:shadow-none
+                        focus:border-green-400
+                        "
+                    />
+                  </div>
+                  <div class="mb-6">
+                    <input
+                      type="email"
+                      name="email"
+                      value={inputs.email || ""}
+                      onChange={handleChange}
+                      placeholder="Email"
+                      required
+                      className="
+                        w-full
+                        rounded
+                        py-3
+                        px-[14px]
+                        text-body-color text-base
+                        border border-[lightgray]
+                        outline-none
+                        focus-visible:shadow-none
+                        focus:border-green-400
+                        "
+                    />
+                  </div>
+                  <div class="mb-6">
+                    <input
+                      type="phone"
+                      name="phone"
+                      value={inputs.phone || ""}
+                      onChange={handleChange}
+                      placeholder="Contact No."
+                      required
+                      className="
+                        w-full
+                        rounded
+                        py-3
+                        px-[14px]
+                        text-body-color text-base
+                        border border-[lightgray]
+                        outline-none
+                        focus-visible:shadow-none
+                        focus:border-green-400
+                        "
+                    />
+                  </div>
+                  <div class="mb-6">
+                    <textarea
+                      name="text"
+                      value={inputs.text || ""}
+                      onChange={handleChange}
+                      id=""
+                      cols="30"
+                      rows="4"
+                      placeholder="Your Message"
+                      required
+                      className="
+                        w-full
+                        rounded
+                        py-3
+                        px-[14px]
+                        text-body-color text-base
+                        border border-[lightgray]
+                        resize-none
+                        outline-none
+                        focus-visible:shadow-none
+                        focus:border-green-400
+                        "
+                    ></textarea>
+                  </div>
+                  <div>
+                    <button
+                      type="submit"
+                      onClick={done}
+                      className="
+                        w-full
+                        text-white
+                        bg-green-400
+                        rounded
+                        border border-green-400
+                        p-3
+                        transition
+                        hover:bg-opacity-90
+                        "
+                    >
+                      Send Message
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="w-full h-screen grid grid-rows-5 md:grid-cols-2 mt-16 md:mt-0 items-center justify-center justify-items-center" >
                 <div className="md:text-6xl text-4xl font-extrabold flex-wrap text-green-400 mx-5 md:row-start-2 ">
                     Contact Me<hr className="mt-3 w-16 h-2 bg-green-400 rounded-full " />
                     <div className=" bg-fixed bg-contactBg w-full"></div>
@@ -49,27 +198,46 @@ export default function contact() {
                     </form>
                 </div>
 
-            </div>
-            <div className=" w-full grid md:grid-rows-5 justify-items-center">
-                <div className="xl:text-6xl lg:text-5xl text-4xl font-extrabold text-green-400 mx-5 self-center place-self-center justify-self-center mt-0 md:mt-4">Contacts<hr className=" w-16 mb-4 md:mb-0 h-2 bg-green-400 rounded-full " /></div>
-                <div className="grid md:grid-cols-2 grid-cols-1 md:row-span-2 gap-4">
-                    <div className="grid gap-4">
-                        <Contactcard text="Facebook" srce="https://www.facebook.com/kamrulhassan.rayhan.7/" imgSrc="https://img.icons8.com/office/40/000000/facebook.png" />
-                        <Contactcard text="Linkedin" srce="https://www.linkedin.com/in/kamrul-hasan-rayhan-76a42917b/" imgSrc="https://img.icons8.com/officel/40/000000/linkedin.png" />
-                    </div>
-                    <div className="grid gap-4">
-                        <Contactcard text="Stack Overflow" srce="https://stackoverflow.com/users/16314541/rayhan" imgSrc="https://img.icons8.com/color/48/000000/stackoverflow.png" />
-                        <Contactcard text="Github" srce="https://github.com/K-H-Rayhan?tab=repositories" imgSrc="https://img.icons8.com/nolan/48/github.png" />
-                    </div>
-                </div>
-            </div>
-        </Layout>
-    )
+            </div> */}
+      <div className=" grid md:grid-rows-4 justify-items-center">
+        <div className="xl:text-6xl lg:text-5xl text-4xl font-extrabold text-green-400 mx-5 self-center place-self-center justify-self-center mt-0 md:mt-4">
+          Contacts
+          <hr className=" w-16 mb-4 md:mb-0 h-2 bg-green-400 rounded-full " />
+        </div>
+        <div className="grid md:grid-cols-2 grid-cols-1 md:row-span-2 gap-4">
+          <div className="grid gap-4">
+            <Contactcard
+              text="Facebook"
+              srce="https://www.facebook.com/kamrulhassan.rayhan.7/"
+              imgSrc="https://img.icons8.com/office/40/000000/facebook.png"
+            />
+            <Contactcard
+              text="Linkedin"
+              srce="https://www.linkedin.com/in/kamrul-hasan-rayhan-76a42917b/"
+              imgSrc="https://img.icons8.com/officel/40/000000/linkedin.png"
+            />
+          </div>
+          <div className="grid gap-4">
+            <Contactcard
+              text="Stack Overflow"
+              srce="https://stackoverflow.com/users/16314541/rayhan"
+              imgSrc="https://img.icons8.com/color/48/000000/stackoverflow.png"
+            />
+            <Contactcard
+              text="Github"
+              srce="https://github.com/K-H-Rayhan?tab=repositories"
+              imgSrc="https://img.icons8.com/nolan/48/github.png"
+            />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
 export async function getStaticProps(context) {
-    const res = fetch('https://khrayhan.me/api/contact');
-    return {
-      props: {}, // will be passed to the page component as props
-    }
-  }
+  const res = fetch("https://khrayhan.me/api/contact");
+  return {
+    props: {}, // will be passed to the page component as props
+  };
+}
